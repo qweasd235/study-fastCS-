0b32d010658801be4ab064e79c1e48c4feda9617
UK computer <UK computer@DESKTOP-1FUPKJF>
2021-07-03 23:54:09 +0900

    객체 지향 공부

---
+++ chapter3/src/ch02/Customer.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class Customer {
+
+	private int customerID;
+	private String customerName;
+	private String customerGrade;
+	int bonusPoint;
+	double bonusRatio;
+	
+	public Customer() {
+		
+	}
+}
