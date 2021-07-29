bdcc763a486dfc281694389471cf7d6b909dc4ec
UK computer <UK computer@DESKTOP-1FUPKJF>
2021-07-29 22:29:07 +0900

    String, StringBuilder, StringBuffer 클래스, text block

---
+++ chapter04/src/ch03/StringBuilderTest.java
@@ -0,0 +1,17 @@
+package ch03;
+
+public class StringBuilderTest {
+
+	public static void main(String[] args) {
+		String java = new String("java");
+		String android = new String("android");
+		
+		StringBuilder builder = new StringBuilder(java);
+		System.out.println(System.identityHashCode(builder));
+		builder.append(android);
+		System.out.println(System.identityHashCode(builder));
+		
+		String test = builder.toString();
+		System.out.println(test);
+	}
+}
